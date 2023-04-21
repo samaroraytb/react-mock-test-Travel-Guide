27bfeaefadb51e30e4c9b728ee5130e9200f67cd
Shubham <samaroraytb@gmail.com>
2023-04-21 11:25:08 +0000

    Travel App

---
+++ src/components/TravelGuide/styledComponents.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+
+export const BgContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #eef4f7;
+  min-height: 100vh;
+`
+
+export const MainHeading = styled.h1`
+  text-align: center;
+  color: #334155;
+  font-family: 'Roboto';
+  font-weight: bold;
+  font-size: 32px;
+  border-bottom: 3px solid #52bbf0;
+  padding-bottom: 10px;
+  margin: 20px 0px 20px 0px;
+`
+
+export const TravelGuideList = styled.ul`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  margin: 20px 10px 20px 10px;
+`
+
+export const LoaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
---
+++ src/components/TravelPackageItem/styledComponents.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components'
+
+export const TravelPackageListItem = styled.li`
+  list-style-type: none;
+  width: 45%;
+  margin-right: 15px;
+  margin-bottom: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #ffffff;
+  @media all and (max-width: 760px) {
+    width: 100%;
+    margin: 10px;
+  }
+`
+
+export const TravelPackageImage = styled.img`
+  @media all and (max-width: 760px) {
+    width: 100%;
+  }
+`
+
+export const TravelInfoContainer = styled.div`
+  padding: 15px;
+`
+
+export const TravelPackageName = styled.h1`
+  color: #475569;
+  font-family: 'Roboto';
+  font-weight: 600;
+  font-size: 22px;
+`
+
+export const TravelPackageInfo = styled.p`
+  color: #64748b;
+  font-family: 'Roboto';
+  font-weight: 400;
+  font-size: 16px;
+`
